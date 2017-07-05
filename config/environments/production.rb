28a7997477d6890f34a40a578726719393747d73
Mark Abella <mark.abella@gmail.com>
2017-07-05 11:32:13 +0000

    add heroku address as host for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'blocipedia.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'alluring-lake-clark-12515.herokuapp.com' }
  
 end

29719a8f7853cd2069d25dbca9ba6ec0c46b16b3
Mark Abella <mark.abella@gmail.com>
2017-07-07 11:17:33 +0000

    Add seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#include RandomData
+ 
+ # Create Users
+ 5.times do
+   user = User.new(
+
+   #name:     RandomData.random_name,
+   email:    Faker::Internet.email,     #RandomData.random_email,
+   password: Faker::Internet.password   #RandomData.random_sentence
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+
+ # Create Wikis
+ 50.times do
+   Wiki.create!(
+     title:  Faker::Lorem.sentence,     #RandomData.random_sentence,
+     body:   Faker::Lorem.paragraphs    #RandomData.random_paragraph
+   )
+ end
+ wikis = Wiki.all
+ 
+ # Create an admin user
+ admin = User.new(
+  # name:     'Admin User',
+   email:    'admin@email.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+  # Create an premium user
+ premium = User.new(
+   #name:     'Admin User',
+   email:    'premium@email.com',
+   password: 'helloworld',
+   role:     'premium'
+ )
+ premium.skip_confirmation!
+ premium.save!
+
+ 
+ # Create a member
+ member = User.new(
+   #name:     'Member User',
+   email:    'member@email.com',
+   password: 'helloworld'
+ )
+ member.skip_confirmation!
+ member.save!
+
+ 
+ puts "Seed finished"
+ puts "#{Wiki.count} wikis created"
+ puts "#{User.count} users created"
